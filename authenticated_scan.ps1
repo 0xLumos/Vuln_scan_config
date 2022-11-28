3485c99a796cbb822112f2417673de8f3eb954aa
alhousen <86202341+alhousen@users.noreply.github.com>
2022-11-28 14:59:52 +0000

    Create local keys for pre scan and firewall shut

--- authenticated_scan.ps1
+++ authenticated_scan.ps1
@@ -1,4 +1,3 @@
-
 <#
 
   
@@ -13,34 +12,79 @@
 .NOTES   
     Name: Enable-RemoteRegistry
     Author: Provention Ltd
-    Version: 1.1
+    Version: 1.2
     DateCreated: 15/11/2022
     DateUpdated: 15/11/2022
 
 #>
 
+echo "Setting remote registery to automatic, start service"
+
+Set-Service -Name RemoteRegistry  -StartupType Automatic -ErrorAction Stop
+
+Start-Service -InputObject (Get-Service -Name RemoteRegistry) -ErrorAction Stop
 
 
-function Pre_Scan {
-#Display all keys and values in this registery 
-	Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\
+echo "-------------------------------------------------------------------------"
 
-	Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ # -name "LocalAccountToken" -> to access a specific key
-#STEP1: Local account token sat to 0
-	New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value ”1"  -PropertyType "DWORD"
-#STEP2: forceguest registery sat to 1 
-	New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value ”0"  -PropertyType "DWORD"
-#STEP3: Local service sat to READ
-	New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg -Name "Local Service" -Value ”READ"  -PropertyType "String"
-#STEP4: Administrators have full control 
-	New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg -Name "Administrators" -Value ”FULL CONTROL"  -PropertyType "String"
-#STEP5: Change remote registery and WMI service startup type to Automatic and start that service
-  Set-Service -Name RemoteRegistry  -StartupType Automatic -ErrorAction Stop
-  Start-Service -InputObject (Get-Service -Name RemoteRegistry) -ErrorAction Stop
-  #Set-Service -Name RemoteRegistry  -StartupType Automatic -ErrorAction Stop
-  #Start-Service -InputObject (Get-Service -Name RemoteRegistry) -ErrorAction Stop
-#STEP6: ALLOW ACCESS TO WMI THROUGH DEFENDER
-  if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$name`"" -Verb RunAs; exit } 
-  netsh advfirewall firewall set rule group="windows management instrumentation (wmi)" new enable=yes # Run as administrator
+if(Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -name LocalAccountToken)
+{
+   echo "Setting LocalAccountToken to 1 "
+   Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1" 
+
+   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
 }
-Pre_Scan
+
+else
+{
+   echo "Creating LocalAccountToken"
+   New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1"  -PropertyType "DWORD"
+   echo "Item has been set"
+   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+}
+
+
+echo "-------------------------------------------------------------------------"
+
+
+
+if( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD")
+   echo "Creating HK "
+   Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1" 
+
+   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken -name "LocalAccountToken" -> to access a specific key
+}
+
+else
+{
+   echo "Setting forceguest"
+   New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD"
+   echo "Item has been set"
+   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+}
+
+echo "-------------------------------------------------------------------------"
+
+if( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD")
+   echo "Creating HK "
+   Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1" 
+
+   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken -name "LocalAccountToken" -> to access a specific key
+}
+
+else
+{
+   echo "Setting forceguest"
+   New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD"
+   echo "Item has been set"
+   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+}
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$name`"" -Verb RunAs; exit } 
+  netsh advfirewall firewall set rule group="windows management instrumentation (wmi)" new enable=yes # Run as administrator
+
+
+
+
+
+
+
