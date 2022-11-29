1f39c22751635b1a0cb2b3774eb2e7dcdc40480a
alhousen <86202341+alhousen@users.noreply.github.com>
2022-11-29 15:06:32 +0000

    Creates enable() function

--- authenticated_scan.ps1
+++ authenticated_scan.ps1
@@ -18,66 +18,66 @@
     DateUpdated: 15/11/2022
 
 #>
+function enable{
+  echo "Setting remote registery to automatic, start service"
 
-echo "Setting remote registery to automatic, start service"
+  Set-Service -Name RemoteRegistry  -StartupType Automatic -ErrorAction Stop
 
-Set-Service -Name RemoteRegistry  -StartupType Automatic -ErrorAction Stop
+  Start-Service -InputObject (Get-Service -Name RemoteRegistry) -ErrorAction Stop
 
-Start-Service -InputObject (Get-Service -Name RemoteRegistry) -ErrorAction Stop
+  echo "-------------------------------------------------------------------------"
 
-echo "-------------------------------------------------------------------------"
+  echo "Setting WMI to automatic, start service"
 
-echo "Setting WMI to automatic, start service"
+  $s = Get-Service wmi
+  Start-Service -InputObject $s -PassThru | Format-List >> services.txt
 
-$s = Get-Service wmi
-Start-Service -InputObject $s -PassThru | Format-List >> services.txt
+  echo "-------------------------------------------------------------------------"
 
-echo "-------------------------------------------------------------------------"
+  if(Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -name LocalAccountToken)
+  {
+     echo "Setting LocalAccountToken to 1 "
+     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1" 
 
-if(Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -name LocalAccountToken)
-{
-   echo "Setting LocalAccountToken to 1 "
-   Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1" 
+     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+  }
 
-   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+  else
+  {
+     echo "Creating LocalAccountToken"
+     New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1"  -PropertyType "DWORD"
+     echo "Item has been set"
+     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+  }
+
+
+  echo "-------------------------------------------------------------------------"
+
+
+
+  if( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD")
+     echo "Creating HK "
+     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "0" 
+
+     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken -name "LocalAccountToken" -> to access a specific key
+  }
+
+  else
+  {
+     echo "Setting forceguest"
+     New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD"
+     echo "Item has been set"
+     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+  }
+
+  echo "-------------------------------------------------------------------------"
+
+
+  if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$name`"" -Verb RunAs; exit } 
+    netsh advfirewall firewall set rule group="windows management instrumentation (wmi)" new enable=yes # Run as administrator
 }
 
-else
-{
-   echo "Creating LocalAccountToken"
-   New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1"  -PropertyType "DWORD"
-   echo "Item has been set"
-   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
-}
-
-
-echo "-------------------------------------------------------------------------"
-
-
-
-if( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD")
-   echo "Creating HK "
-   Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "0" 
-
-   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken -name "LocalAccountToken" -> to access a specific key
-}
-
-else
-{
-   echo "Setting forceguest"
-   New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD"
-   echo "Item has been set"
-   Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
-}
-
-echo "-------------------------------------------------------------------------"
-
-
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$name`"" -Verb RunAs; exit } 
-  netsh advfirewall firewall set rule group="windows management instrumentation (wmi)" new enable=yes # Run as administrator
-
-
-
+enable
 
 
 
