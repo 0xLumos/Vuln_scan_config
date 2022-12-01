cab1c77c8f847c05363794f6a4094c406ecc1b40
alhousen <86202341+alhousen@users.noreply.github.com>
2022-12-01 20:08:29 +0000

    Fixes major issues

--- authenticated_scan.ps1
+++ authenticated_scan.ps1
@@ -1,5 +1,4 @@
 <#
-
   
   
  
@@ -14,11 +13,10 @@
 .NOTES   
     Name: Authenticated scan
     Author: Nour Alhouseini | Provention Ltd
-    Version: 1.7
+    Version: 2.0
     DateCreated: 15/11/2022
-    DateUpdated: 29/11/2022
+    DateUpdated: 01/12/2022
      Github raw script : https://raw.githubusercontent.com/alhousen/Provention-/main/authenticated_scan.ps1
-
 #>
 function enable{
   echo "Setting remote registery to automatic, start service"
@@ -29,12 +27,33 @@ function enable{
 
   echo "-------------------------------------------------------------------------"
 
+
   echo "Setting WMI to automatic, start service"
 
-  $s = Get-Service wmi
-  Start-Service -InputObject $s -PassThru | Format-List >> services.txt
-
+  #https://learn.microsoft.com/en-us/windows/win32/wmisdk/starting-and-stopping-the-wmi-service
+  net start winmgmt
   echo "-------------------------------------------------------------------------"
+
+
+#\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\
+# https://www.stigviewer.com/stig/windows_7/2017-02-21/finding/V-21950
+ if(Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\ -name SmbServerNameHardeningLevel)
+  {
+     echo "Setting SmbServerNameHardeningLevel to 1 "
+     Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\ -Name "SmbServerNameHardeningLevel" -Value "1" 
+
+     Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\ | findstr SmbServerNameHardeningLevel # -name "LocalAccountToken" -> to access a specific key
+  }
+
+  else
+  {
+     echo "Creating SmbServerNameHardeningLevel"
+     New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\ -Name "SmbServerNameHardeningLevel" -Value "1"  -PropertyType "DWORD"
+     echo "SmbServerNameHardeningLevel has been set"
+     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr SmbServerNameHardeningLevel # -name "LocalAccountToken" -> to access a specific key
+  }
+
+
 
   if(Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -name LocalAccountToken)
   {
@@ -48,7 +67,7 @@ function enable{
   {
      echo "Creating LocalAccountToken"
      New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "1"  -PropertyType "DWORD"
-     echo "Item has been set"
+     echo "LocalAccountToken has been set"
      Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
   }
 
@@ -57,20 +76,20 @@ function enable{
 
 
 
-  if( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD")
+  if( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" )
   {
-     echo "Creating HK "
-     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "LocalAccountToken" -Value "0" 
+     echo "Creating forceguest "
+     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -Name "forceguest" -Value "0" 
 
-     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken -name "LocalAccountToken" -> to access a specific key
+     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr forceguest  #-> to access a specific key
   }
 
   else
   {
      echo "Setting forceguest"
      New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name "forceguest" -Value "0"  -PropertyType "DWORD"
-     echo "Item has been set"
-     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr LocalAccountToken # -name "LocalAccountToken" -> to access a specific key
+     echo "forceguest has been set"
+     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ | findstr forceguest # -name "LocalAccountToken" -> to access a specific key
   }
 
   echo "-------------------------------------------------------------------------"
@@ -78,6 +97,8 @@ function enable{
 
   if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$name`"" -Verb RunAs; exit } 
     netsh advfirewall firewall set rule group="windows management instrumentation (wmi)" new enable=yes # Run as administrator
+    #rules enable = yes
+  echo "Exiting..."
 }
 
 
@@ -92,27 +113,32 @@ function disable{
 
   echo "-------------------------------------------------------------------------"
 
+  
   echo "Stopping WMI service"
 
-  $s = Get-Service wmi
-  Stop-Service -InputObject $s -PassThru
+
+
+  #$s = Get-Service wmi
+  #Stop-Service -InputObject $s -PassThru
 
   echo "-------------------------------------------------------------------------"
 
   Clear-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -name LocalAccountToken
-
+  echo "LocalAccountToken deleted.."
  
 
   Clear-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\ -name forceguest
+  echo "forceguest deleted.."
+
 
  
-
- 
-  echo "-------------------------------------------------------------------------"
+  
 
 
   if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$name`"" -Verb RunAs; exit } 
     netsh advfirewall firewall set rule group="windows management instrumentation (wmi)" new enable=no # Run as administrator
+    #rules enable = no
+  echo "Exiting..."
 }
 
 
@@ -122,12 +148,6 @@ $param1=$args[0]
 if ($param1 -eq 'enable'){
     enable
 }
-elseif($param1 -ne 'disable') {
+elseif($param1 -eq 'disable') {
    disable
 }
-
-
-
-
-
-
